13d63183a03bf8e39431f85c733f5071e5ceecd1
Angel Zamora Martínez <azamora@adherahealth.com>
2025-12-23 18:04:30 +0000

    Quitar filtrado de evaluaciones activas en vista de evaluaciones. Ajuste de reglas de acceso

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,22 +1,3 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
-
-
-# class AulaMetrics(http.Controller):
-#     @http.route('/aula_metrics/aula_metrics', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/aula_metrics/aula_metrics/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('aula_metrics.listing', {
-#             'root': '/aula_metrics/aula_metrics',
-#             'objects': http.request.env['aula_metrics.aula_metrics'].search([]),
-#         })
-
-#     @http.route('/aula_metrics/aula_metrics/objects/<model("aula_metrics.aula_metrics"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('aula_metrics.object', {
-#             'object': obj
-#         })
+# Placeholder para controladores futuros
 
