a39693b1523750795a4fb81834624ed0e96ccc0c
Angel Zamora Martínez <azamora@adherahealth.com>
2026-05-16 13:33:21 +0000

    campo de edad recomendada yd escripciopn de medidas

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "AulaMetrics",
-    'version': '1.0.8',
+    'version': '1.0.9',
     'summary': "Sistema de evaluación psicosocial para centros educativos",
 
     'author': "Angel Zamora",
